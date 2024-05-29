3ccbf69992b309ee3d015e839f972fe55577c440
Vishal Kalita <vishalkalita036@gmail.com>
2024-05-29 16:46:59 +0530

    overlay updates

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,6 @@ export default function Home() {
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
-        {/* <AchievementsSection/> */}
         <AboutSection />
         <ProjectSection />
         <EmailSection />
